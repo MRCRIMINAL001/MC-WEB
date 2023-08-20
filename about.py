05cb9175d33df5b9652ebeed1fc932ca22519007
Albert Tomas <117319954+MrcriminalS3UX@users.noreply.github.com>
2023-08-21 04:39:17 +0600

    Update about.py

--- about.py
+++ about.py
@@ -5,19 +5,19 @@ os.system('clear')
 text = ("Script Writter :")
 def about():
     print """
-\033[1;32mAuthor  : \033[1;33mTeam DarkWeb T-D
-\033[1;32mGROUP   : \033[1;33mhttps://facebook.com/groups/368007073698875
+\033[1;32mAuthor  : \033[1;33mShadow Force
+\033[1;32mGROUP   : \033[1;33mhttps://facebook.com/groups/Shadowforceofficial/
 
 \033[1;31m××× You Can't Expect To  See Change ! 
         If You Never Do Anything Differently ×××
 
 \033[1;31m☆ \033[1;32mScript Writter :
 
-\033[1;31m   <<< \033[1;33mNabil-Rahman \033[1;31m>>>
+\033[1;31m   <<< \033[1;33mAnik-Hossen \033[1;31m>>>
 
 \033[1;31m☆ \033[1;32mThanks To :
 
-\033[1;31m> \033[1;33mAll  (Team DarkWeb) Members
+\033[1;31m> \033[1;33mAll  (Shadow Force) Members
 """
 
 logo = """\033[1;32m
